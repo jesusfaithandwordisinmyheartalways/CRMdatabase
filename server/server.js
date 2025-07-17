6b43b65120ee4613676080ab751c613d5231d933
Andrew Johnson <90214404+jesusfaithandwordisinmyheartalways@users.noreply.github.com>
2025-07-17 10:29:42 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // your React app origin
+    origin: 'https://crmdatabaseclient.onrender.com', // your React app origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
